12387d0db8f7f6a77b3355a9bba848dbe51f8871
James Hatfield <james.hatfield17@icloud.com>
2025-01-05 15:45:07 +0000

    Initialize JS file, pseudocode for first functions added

---
+++ calculator.js
@@ -0,0 +1,11 @@
+// Entered numbers are here
+let enteredNumbers = [];
+
+// Adding addition function
+// First iteration will parse an array, which enables greater scaleability than taking a number of nums
+
+// Adding subtraction function
+
+// Adding division function
+
+// Adding multiplication function
