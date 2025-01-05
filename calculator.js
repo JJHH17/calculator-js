c3abc7ba0e806afdd40e97612284a0cb111079be
James Hatfield <james.hatfield17@icloud.com>
2025-01-05 20:26:25 +0000

    Add subbedNums function

--- calculator.js
+++ calculator.js
@@ -1,16 +1,24 @@
 // Adding addition function
 // First iteration will parse an array, which enables greater scaleability than taking a number of nums
 function summedNums(...numbers) {
-    total = 0;
+    let total = 0;
     for (let i = 0; i < numbers.length; i++) {
         total += numbers[i];
     }
-    console.log(total);
-}
-// Refactor above using a method later 
+    return total;
+} // Refactor above using a method later 
+
 
 // Adding subtraction function
+function subbedNums(...numbers) {
+    let total = numbers[0];
+    for (let i = 1; i < numbers.length; i++) {
+        total -= numbers[i];
+    }
+    console.log(total);
+} // Refactor using an array method later
 
+subbedNums(10, 5); 
 // Adding division function
 
 // Adding multiplication function
