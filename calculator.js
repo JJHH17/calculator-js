bc2702b6dbf13b0c0d693f4871fbed6320d0a3a9
James Hatfield <james.hatfield17@icloud.com>
2025-01-05 20:10:02 +0000

    Add first iteration of summedNums function

--- calculator.js
+++ calculator.js
@@ -1,5 +1,13 @@
 // Adding addition function
 // First iteration will parse an array, which enables greater scaleability than taking a number of nums
+function summedNums(...numbers) {
+    total = 0;
+    for (let i = 0; i < numbers.length; i++) {
+        total += numbers[i];
+    }
+    console.log(total);
+}
+// Refactor above using a method later 
 
 // Adding subtraction function
 
