f80e28174dac96aca9abc4b092bdb6e0f7d80228
James Hatfield <james.hatfield17@icloud.com>
2025-01-06 20:47:49 +0000

    Initialize button event listener creation

--- calculator.js
+++ calculator.js
@@ -9,7 +9,6 @@ function summedNums(number1, number2) {
     return number1 + number2;
 } 
 
-
 // Adding subtraction function
 function subbedNums(number1, number2) {
     return number1 - number2; 
@@ -23,7 +22,6 @@ function diviNums(number1, number2) {
         return number1 / number2;
     }
 }
-
 
 // Adding multiplication function
 function multiNums(number1, number2) {
@@ -42,3 +40,27 @@ function operate(number1, operator, number2) {
         multiNums(number1, number2);
     }
 }
+
+// Adding event listeners for calculator buttons
+const one = document.querySelector("#one");
+// add event of clicking button 
+const two = document.querySelector("#two");
+// add event of clicking button
+const three = document.querySelector("#three");
+// add event of clicking button
+const four = document.querySelector("#four");
+// add event of clicking button 
+const five = document.querySelector("#five");
+// add event of clicking button
+const six = document.querySelector("#six");
+// add event of clicking button 
+const seven = document.querySelector("#seven");
+// add event of clicking button
+const eight = document.querySelector("#eight");
+// add event of clicking button
+const nine = document.querySelector("#nine");
+// add event of clicking button
+const zero = document.querySelector("#zero");
+// Add event of clicking button
+
+// Add operator buttons 
